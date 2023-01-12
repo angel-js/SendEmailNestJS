9913975a70c735b289b0f8208a11bf4a71201f8a
angel-js <angelserrano143@gmail.com>
2023-01-11 21:36:05 -0300

    feat(): 1.2 Send Message with the service divided in this Module

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -2,12 +2,21 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer/dist';
 //import { CreateEmailDto } from './dto/create-email.dto';
 //import { UpdateEmailDto } from './dto/update-email.dto';
+import { EmailService } from './email.service';
 
 @Controller('email')
 export class EmailController {
-  constructor(private readonly mailService: MailerService) {}
-
+  constructor(private readonly emailService: EmailService) {}
   @Get('send')
+  plainTextEmail(
+    @Query('toemail') toemail,
+    @Query('sub') sub,
+    @Query('message') message,
+  ) {
+    return this.emailService.sendEmail(toemail, sub, message);
+  }
+
+  /* @Get('send')
   async plainTextEmail(
     @Query('toemail') toemail,
     @Query('sub') sub,
@@ -22,16 +31,5 @@ export class EmailController {
     });
     return 'success';
   }
-
-  /*   @Get('send')
-  async plainTextEmail(@Query('toemail') toemail) {
-    console.log('Afuera de la funcion');
-    await this.mailService.sendMail({
-      to: toemail,
-      from: 'emailparaejerciciodenestprueba@gmail.com',
-      subject: 'Este es un mensaje de Prueba',
-      text: 'Estimados envio un cordial saludo mediante este instrumento! Mail Automatico :D',
-    });
-    return 'success';
-  } */
+ */
 }
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer/dist';
 //import nodemailer from 'nodemailer';
 //import { main } from './enviarMail';
 //import { CreateEmailDto } from './dto/create-email.dto';
@@ -6,7 +7,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class EmailService {
-  async sendEmail() {
-    return `This action returns all Emails`;
+  constructor(private readonly mailService: MailerService) {}
+
+  async sendEmail(toemail: string, sub: string, message: string) {
+    await this.mailService.sendMail({
+      to: toemail,
+      from: 'emailparaejerciciodenestprueba@gmail.com',
+      subject: sub,
+      text: message,
+    });
+    return `The Message has been send successfully`;
   }
 }
